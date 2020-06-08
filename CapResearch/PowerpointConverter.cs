eeedabc9bcd56a3fb358ffeda79d3c4273c6c1d1
julian-heng <julianhengwl@gmail.com>
2020-06-08 18:42:09 +0800

    Add error handling if MS Office is not installed

--- CapResearch/PowerpointConverter.cs
+++ CapResearch/PowerpointConverter.cs
@@ -9,17 +9,27 @@ namespace CapResearch
     {
         public static void Convert(string input, string output)
         {
-            var pptApp = new PowerPoint.Application();
-            var powerpointDocument = pptApp.Presentations.Open(input,
-                MsoTriState.msoTrue, //ReadOnly
-                MsoTriState.msoFalse, //Untitled
-                MsoTriState.msoFalse); //Window not visible during converting
-           
-            powerpointDocument.ExportAsFixedFormat(output, 
-                PowerPoint.PpFixedFormatType.ppFixedFormatTypePDF);
-
-            powerpointDocument.Close(); //Close document
-            pptApp.Quit(); //Important: When you forget this PowerPoint keeps running in the background
+            PowerPoint.Application pptApp = null;
+            try
+            {
+                pptApp = new PowerPoint.Application();
+                var powerpointDocument = pptApp.Presentations.Open(input,
+                    MsoTriState.msoTrue, //ReadOnly
+                    MsoTriState.msoFalse, //Untitled
+                    MsoTriState.msoFalse); //Window not visible during converting
+                powerpointDocument.ExportAsFixedFormat(output,
+                    PowerPoint.PpFixedFormatType.ppFixedFormatTypePDF);
+                powerpointDocument.Close(); //Close document
+            }
+            catch (COMException e)
+            {
+                Console.WriteLine("Microsoft PowerPoint is not installed...");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                pptApp?.Quit(); //Important: When you forget this PowerPoint keeps running in the background
+            }
         }
     }
 }
--- CapResearch/WordConverter.cs
+++ CapResearch/WordConverter.cs
@@ -8,28 +8,29 @@ namespace CapResearch
     {
         public static void Convert(string input, string output)
         {
+            Word.Application wordApp = null;
             try
             {
-                var wordApp = new Word.Application();
-                wordApp.Visible = false;
-                
-                var wordDoc = wordApp.Documents.Open(input,
-                    ReadOnly: true); // Open in readonly
-                
-                Word.WdExportOptimizeFor viewQuality = Word.WdExportOptimizeFor.wdExportOptimizeForOnScreen;
-            
+                wordApp = new Word.Application
+                {
+                    Visible = false
+                };
+
+                var wordDoc = wordApp.Documents.Open(input, ReadOnly: true); // Open in readonly
+                var viewQuality = Word.WdExportOptimizeFor.wdExportOptimizeForOnScreen;
                 wordDoc.ExportAsFixedFormat(output, Word.WdExportFormat.wdExportFormatPDF, false, viewQuality);
-
-                wordDoc.Close(Word.WdSaveOptions.wdDoNotSaveChanges, 
-                    Word.WdOriginalFormat.wdOriginalDocumentFormat, 
+                wordDoc.Close(Word.WdSaveOptions.wdDoNotSaveChanges,
+                    Word.WdOriginalFormat.wdOriginalDocumentFormat,
                     false); //Close document
-
-                wordApp.Quit(); //Important: When you forget this Word keeps running in the background
             }
             catch (COMException e)
             {
-                Console.WriteLine(e);
-                throw;
+                Console.WriteLine("Microsoft Word is not installed...");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                wordApp?.Quit();  //Important: When you forget this Word keeps running in the background
             }
         }
     }
